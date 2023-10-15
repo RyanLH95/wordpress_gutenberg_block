f4950bac8b8c039dfa51c76888b0b4fd2b9b238a
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 17:07:34 +0100

    Create attribute functions for where the post information will be stored

--- post-search-block/src/block.js
+++ post-search-block/src/block.js
@@ -3,9 +3,29 @@ registerBlockType( 'cgb/post-search-block' ), {
 	title: __( 'Load a Post' ), // Block title.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
-		__( 'load' ),
-		__( 'Load Post' ),
-		__( 'post-search-block' ),
-	],
+        __( 'my-block — CGB Block' ),
+        __( 'CGB Example' ),
+        __( 'create-guten-block' ),
+      ],
+
+    attributes: {
+        content: {
+          type: 'array',
+          source: 'children',
+          selector: 'p',
+        },
+        title: {
+          type: 'string',
+          selector: 'h2'
+        },
+        link: {
+          type: 'string',
+          selector: 'a'
+        },
+        selectedPost: {
+          type: 'number',
+          default: 0,
+        },
+      },
   // ...
 }
