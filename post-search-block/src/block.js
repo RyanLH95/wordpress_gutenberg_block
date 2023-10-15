1fb30148a35c3fe9caaaded077046753b707560e
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 17:02:06 +0100

    Create block.js in src directory and make this the main focus for code for gutenberg block

---
+++ post-search-block/src/block.js
@@ -0,0 +1,11 @@
+registerBlockType( 'cgb/post-search-block' ), {
+	// Block name.
+	title: __( 'Load a Post' ), // Block title.
+	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	keywords: [
+		__( 'load' ),
+		__( 'Load Post' ),
+		__( 'post-search-block' ),
+	],
+  // ...
+}
