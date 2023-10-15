96c40c8bc5fbd1f4c6e79584f9d2f8c57b789c1a
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 16:53:35 +0100

    Add code snippet for pagination for post within post-search-block.php file

--- post-search-block/post-search-block.php
+++ post-search-block/post-search-block.php
@@ -23,6 +23,26 @@
 function create_block_post_search_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
+
+global $wp_query;
+$total = $wp_query->max_num_pages;
+// only bother with the rest if we have more than 1 page!
+if ( $total > 1 ) {
+     // get the current page
+     if ( !$current_page = get_query_var('paged') )
+          $current_page = 1;
+     // structure of "format" depends on whether we're using pretty permalinks
+     $format = empty( get_option('permalink_structure') ) ? '&page=%#%' : 'page/%#%/';
+     echo paginate_links(array(
+          'base' => get_pagenum_link(1) . '%_%',
+          'format' => $format,
+          'current' => $current_page,
+          'total' => $total,
+          'mid_size' => 4,
+          'type' => 'list'
+     ));
+}
+
 add_action( 'init', 'create_block_post_search_block_block_init' );
 
 add_filter( 'auto_update_plugin', '__return_true' );
